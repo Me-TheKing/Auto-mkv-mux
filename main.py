d2598107ddd35c4fe74e952037c5661967ca2466
Me-TheKing <55807805+Me-TheKing@users.noreply.github.com>
2019-10-06 23:02:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
-import tkinter as tk
-from tkinter import filedialog
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog 
+from UI.maingui import Ui_Form  # importing our generated file 
+import sys
 import os
 import subprocess
+ 
+class MyApp(QtWidgets.QMainWindow, Ui_Form):
+    def __init__(self):
+        super(MyApp, self).__init__() 
+        self.ui = Ui_Form()    
+        self.ui.setupUi(self)
+        ##### my set var ####
+        self.video_ext = ["mp4", "mkv", "avi"]
+        self.sub_ext = ["srt", "ass", "txt", "ssa", "sub"]
+        self.mkvmerge_path = "C:\\Program Files\\MKVToolNix\\mkvmerge.exe"
+
+        self.btn_handler()
 
 
-video_ext = ["mp4", "mkv", "avi"]
-sub_ext = ["srt", "ass", "txt", "ssa", "sub"]
-mkvmerge_path = "C:\\Program Files\\MKVToolNix\\mkvmerge.exe"
+    def btn_handler(self):
+        self.ui.search_btn.clicked.connect(self.btnClicked) # connecting the clicked signal with btnClicked slot
+        self.ui.start_btn.clicked.connect(self.start_mux)
 
-root = tk.Tk()
-root.withdraw()
+    def btnClicked(self):
+        file_path = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.ui.lineEdit.setText(file_path)
+        os.chdir(file_path)
+        my_files = os.listdir(file_path)
+        self.vid_sub_list(my_files)
+        #self.ui.output_txt.setPlainText(str(self.my_files))
 
-file_path = filedialog.askdirectory()
-os.chdir(file_path)
-my_files = os.listdir(file_path)
+    def vid_sub_list(self, my_files):
+        self.video_file = []
+        self.sub_file = []
+        for file in my_files :
+            ext = file[-3:]
+            name = file[:-4]
 
-video_file = []
-sub_file = []
-for file in my_files :
-	ext = file[-3:]
-	name = file[:-4]
+            if ext in self.video_ext :
+                self.video_file.append([name, ext])
+            elif ext in self.sub_ext :
+                self.sub_file.append([name, ext])
 
-	if ext in video_ext :
-		video_file.append([name, ext])
-	elif ext in sub_ext :
-		sub_file.append([name, ext])
+    def start_mux(self):
+        for vid in self.video_file :
+            for sub in self.sub_file :
+                if vid[0] == sub[0] :
+                    original_file = vid[0]+"."+vid[1]
+                    dest_file_path = "New_"+ original_file
+                    full_sub_name = sub[0]+"."+sub[1]
 
-for vid in video_file :
-	for sub in sub_file :
-		if vid[0] == sub[0] :
-			original_file = vid[0]+"."+vid[1]
-			dest_file_path = "New_"+ original_file
-			full_sub_name = sub[0]+"."+sub[1]
+                    returncode = subprocess.Popen(self.mkvmerge_path +' -o "'+ dest_file_path +'" "'+ original_file +'" --forced-track "0:yes" --default-track "0:yes" --track-name "0:Arabic" --language "0:ara" "'+ full_sub_name +'"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+                    stdout_value = returncode.communicate()[0]                       
+                    self.ui.output_txt.setPlainText(stdout_value)
+ 
 
-			returncode = subprocess.run(mkvmerge_path +' -o "'+ dest_file_path +'" "'+ original_file +'" --forced-track "0:yes" --default-track "0:yes" --track-name "0:Arabic" --language "0:ara" "'+ full_sub_name +'"')
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
