aa1cb107b648ae06c9ed366a6a304db8b0ed6697
Me-TheKing <55807805+Me-TheKing@users.noreply.github.com>
2019-10-06 23:04:20 +0300

    Add files via upload

---
+++ maingui.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\H.Ali\Desktop\Auto mux mkv\UI\maingui.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(650, 800)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
+        Form.setAcceptDrops(True)
+        self.search_btn = QtWidgets.QPushButton(Form)
+        self.search_btn.setGeometry(QtCore.QRect(560, 20, 75, 25))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.search_btn.setFont(font)
+        self.search_btn.setObjectName("search_btn")
+        self.lineEdit = QtWidgets.QLineEdit(Form)
+        self.lineEdit.setGeometry(QtCore.QRect(110, 20, 440, 20))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.output_txt = QtWidgets.QPlainTextEdit(Form)
+        self.output_txt.setGeometry(QtCore.QRect(10, 100, 630, 680))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.output_txt.setFont(font)
+        self.output_txt.setObjectName("output_txt")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(10, 20, 95, 20))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.start_btn = QtWidgets.QPushButton(Form)
+        self.start_btn.setGeometry(QtCore.QRect(10, 60, 630, 30))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.start_btn.setFont(font)
+        self.start_btn.setObjectName("start_btn")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Auto add sub to video"))
+        self.search_btn.setText(_translate("Form", "Search"))
+        self.label.setText(_translate("Form", "Video Path Folder :"))
+        self.start_btn.setText(_translate("Form", "Start Mux"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
