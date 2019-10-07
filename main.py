1ca337cb3c4714e3d34b1488ee7fb3df89b66151
Me-TheKing <55807805+Me-TheKing@users.noreply.github.com>
2019-10-07 20:47:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,11 +24,10 @@ class MyApp(QtWidgets.QMainWindow, Ui_Form):
 
     def btnClicked(self):
         file_path = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
-        self.ui.lineEdit.setText(file_path)
+        self.ui.folder_path_LE.setText(file_path)
         os.chdir(file_path)
         my_files = os.listdir(file_path)
-        self.vid_sub_list(my_files)
-        #self.ui.output_txt.setPlainText(str(self.my_files))
+        self.vid_sub_list(my_files)        
 
     def vid_sub_list(self, my_files):
         self.video_file = []
@@ -43,17 +42,47 @@ class MyApp(QtWidgets.QMainWindow, Ui_Form):
                 self.sub_file.append([name, ext])
 
     def start_mux(self):
+        vidsub_lst = []
+
+        try:
+            os.makedirs("Done")
+        except FileExistsError:
+            # directory already exists
+            # i need a massageBox warrning
+            print("ok")
+            pass
+
         for vid in self.video_file :
             for sub in self.sub_file :
                 if vid[0] == sub[0] :
-                    original_file = vid[0]+"."+vid[1]
-                    dest_file_path = "New_"+ original_file
-                    full_sub_name = sub[0]+"."+sub[1]
+                    vidsub_lst.append([vid[0]+"."+vid[1], sub[0]+"."+sub[1]])
 
-                    returncode = subprocess.Popen(self.mkvmerge_path +' -o "'+ dest_file_path +'" "'+ original_file +'" --forced-track "0:yes" --default-track "0:yes" --track-name "0:Arabic" --language "0:ara" "'+ full_sub_name +'"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-                    stdout_value = returncode.communicate()[0]                    
-                    self.ui.output_txt.setPlainText(stdout_value)
-                    QApplication.processEvents()
+        for a_vidsub in vidsub_lst :
+            vid_name = a_vidsub[0]
+            dest_vid_path = "Done/"+ vid_name
+            sub_name = a_vidsub[1]
+            # read the option 
+            option_forced_track = str(self.ui.forced_cbox.currentText())#'yes' # read this from a droplist
+            option_default_track = str(self.ui.default_cbox.currentText())
+            option_track_name = str(self.ui.name_LE.text())
+            option_language = str(self.ui.language_cbox.currentText())
+            option_code = '" --forced-track "0:'+option_forced_track+'" --default-track "0:'+option_default_track+'" --track-name "0:'+option_track_name+'" --language "0:'+option_language+'" '
+
+            returncode = subprocess.Popen(
+                                            self.mkvmerge_path +
+                                            ' -o "' + 
+                                            dest_vid_path +
+                                            '" "' +
+                                            vid_name +
+                                            option_code +
+                                            '"' + sub_name + '"',
+                                             stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+            stdout_value = returncode.communicate()[0]
+            self.ui.output_PTE.setPlainText("vidsub_lst len :"+ str(len(vidsub_lst)))
+            #self.ui.output_PTE.setPlainText("vidsub_lst len :"+ str(len(vidsub_lst)))
+            self.ui.output_PTE.setPlainText(stdout_value)
+            QApplication.processEvents()
+            #QApplication.processEvents()
  
 
 if __name__ == "__main__":
